e1fcfd1d8bc651212617dff279fd9a7f6e6b8664
sachdevs <sachdev.96@gmail.com>
2015-06-13 17:22:21 -0400

    added update functionality

--- cachedTable.js
+++ cachedTable.js
@@ -1,12 +1,14 @@
 function cachedTable(tableName, columns) {
     if (!columns) {
-        if (!localStorage.getItem(tableName))
-            return -1;
+        if (!localStorage.getItem(tableName)) {
+            this.database = undefined;
+            return;
+        }
         this.database = JSON.parse(localStorage.getItem(tableName));
         this.tableName = tableName;
         this.columns = Object.keys(this.database.columns);
         this.rownumber = this.database.rownumber;
-        return 0;
+        return;
     }
     this.database = {};
     this.database.columns = {};
@@ -25,7 +27,7 @@ cachedTable.prototype = {
     constructor: cachedTable,
     addRow: function(row) {
         if (row.length !== this.columns.length)
-            throw "Invalid number of items for row, line 9 cachedTable.js"
+            throw "Invalid number of items for row, line 9 cachedTable.js";
         for (var i = 0; i < this.columns.length; i++) {
             if (!this.database.columns[this.columns[i]].hasOwnProperty(row[i]))
                 this.database.columns[this.columns[i]][row[i]] = [];
@@ -34,7 +36,6 @@ cachedTable.prototype = {
         this.database.allrows.push(row);
         this.rownumber++;
         this.database.rownumber = this.rownumber;
-        console.log(this.database);
         localStorage.setItem(this.tableName, JSON.stringify(this.database));
     },
     getRow: function(column, value) {
@@ -47,8 +48,36 @@ cachedTable.prototype = {
         for (var i = 0; i < getRow.length; i++) {
             ret.push(this.database.allrows[getRow[i]]);
         }
-        console.log(ret);
         return ret;
+    },
+    getRowIds: function(column, value) {
+        if (!this.database.columns.hasOwnProperty(column))
+            throw "column does not exist";
+        if (!this.database.columns[column].hasOwnProperty(value))
+            throw "value does not exist";
+        return this.database.columns[column][value];
+    },
+    updateRows: function(column, value, columnToChange, valueToChange) {
+        if (!this.database.columns.hasOwnProperty(columnToChange))
+            throw "column does not exist";
+        var _row = this.getRowIds(column, value).slice();
+        var column_id = Object.keys(this.database.columns).indexOf(columnToChange);
+        for (var i = 0; i < _row.length; i++) {
+            _value = this.database.allrows[_row[i]][column_id];
+            this.database.allrows[_row[i]][column_id] = valueToChange;
+            var rowIndex = this.database.columns[columnToChange][_value].indexOf(_row[i]);
+            this.database.columns[columnToChange][_value].splice(rowIndex,1);
+            if(this.database.columns[columnToChange][_value].length === 0){
+                delete this.database.columns[columnToChange][_value];
+            }
+            if(!this.database.columns[columnToChange].hasOwnProperty(valueToChange)){
+                this.database.columns[columnToChange][valueToChange] = [];
+                this.database.columns[columnToChange][valueToChange].push(_row[i]);
+            }
+            else
+                this.database.columns[columnToChange][valueToChange].push(_row[i]);
+        }
+        localStorage.setItem(this.tableName, JSON.stringify(this.database));
     },
     _log: function() {
         console.log(localStorage.getItem(this.tableName));
@@ -56,7 +85,7 @@ cachedTable.prototype = {
     _clear: function() {
         localStorage.clear();
     }
-}
+};
 
 // var hi = new cachedTable('lol', ['a', 'b', 'c']);
 
